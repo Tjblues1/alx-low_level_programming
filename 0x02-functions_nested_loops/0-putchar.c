2ef655f3e1b23781467df1d29d9b1d562b935811
Tjblues1 <tjblues4real@yahoo.com>
2022-09-13 16:41:54 -0700

    putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
-* main - prints Putchar
-*
-* Description: prints _putchar
-*
+* main - prints Putchar*
+* Description: prints _putchar*
 * Return: 0 (Success)
 */
 
@@ -18,7 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-_putchar('\n');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
