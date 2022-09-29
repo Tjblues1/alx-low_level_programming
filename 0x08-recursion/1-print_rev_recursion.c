ec7397e01767f9a7e053bcc123125897decf4f65
Tjblues1 <tjblues4real@yahoo.com>
2022-09-29 09:13:03 -0700

    1-print_rev_recursion.c2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,11 @@
-nclude "main.h"
+#include "main.h"
+
 /**
-* _print_rev_recursion - prints a string in reverse
-* @s: string to be printed
-*
+* _print_rev_recursion - function that print a string in reverse
+* @s: string to print
 * Return: void
 */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
