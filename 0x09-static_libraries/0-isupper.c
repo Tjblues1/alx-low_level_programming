7d12c013d212e6d034c6639b44fff633435bc254
Tjblues1 <tjblues4real@yahoo.com>
2022-10-02 09:37:14 -0700

    o-isupper.c1

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,11 @@
-#include "main.h"
+#include "main.h"
 
 /**
-* _isupper - checks if a character is uppercase
-*
-* @d: int is the char
-* Return: 1 if d is uppercase, 0 is otherwise
+* _isupper - checks for uppercase character
+* @c: the character to be checked
+* Return: 1 if c is uppercase, 0 otherwise
 */
-int _isupper(int d)
+int _isupper(int c)
 {
-	int isUpper = 0;
-
-	if (d >= 'A' && d <= 'Z')
-		isUpper = 1;
-
-	return (isUpper);
+	return (c >= 'A' && c <= 'Z');
 }
